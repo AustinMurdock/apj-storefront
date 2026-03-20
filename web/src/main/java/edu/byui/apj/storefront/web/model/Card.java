2abc9b2a99f7a695a61e56fcb6566c1aa1b499da
Austin Murdock <aumu870@gmail.com>
2026-03-19 18:56:43 -0600

    week8 storefront mvc setup
    and static homepage files

---
+++ web/src/main/java/edu/byui/apj/storefront/web/model/Card.java
@@ -0,0 +1,19 @@
+package edu.byui.apj.storefront.web.model;
+
+public class Card {
+    private String name;
+    private String specialty;
+
+    public Card(String name, String specialty) {
+        this.name = name;
+        this.specialty = specialty;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSpecialty() {
+        return this.specialty;
+    }
+}
